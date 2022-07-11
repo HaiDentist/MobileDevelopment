931f8e612dc3bce74d39b9b482a7509477e3d154
Aaksanurirwan <andiaksa16@gmail.com>
2022-07-11 13:06:52 +0800

    Menambahkan Kamera

--- app/src/main/java/com/dicoding/picodiploma/haidentist/ui/camera/camerax/CameraActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/haidentist/ui/camera/camerax/CameraActivity.kt
@@ -1,12 +1,197 @@
 package com.dicoding.picodiploma.haidentist.ui.camera.camerax
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Rational
+import android.view.Surface
+import android.view.View
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 import com.dicoding.picodiploma.haidentist.R
+import com.dicoding.picodiploma.haidentist.databinding.ActivityCameraBinding
+import com.dicoding.picodiploma.haidentist.databinding.LayoutCameraBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
 
 class CameraActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCameraBinding
+    private var imageFile: File? = null
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+    private var imageCapture: ImageCapture? = null
+    private lateinit var cameraBinding: LayoutCameraBinding
+
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_camera2)
+        binding = ActivityCameraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        //Request Camera Permissions
+        if (allPermissionGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+        cameraBinding = binding.cameraView
+        cameraBinding.cameraCaptureButton.setOnClickListener {
+            takePhoto()
+            binding.check.visibility = View.VISIBLE
+            binding.retake.visibility = View.VISIBLE
+            binding.layout.visibility = View.VISIBLE
+        }
+
+        cameraBinding.switchCamera.setOnClickListener {
+            cameraSelector =
+                if (cameraSelector.equals(CameraSelector.DEFAULT_BACK_CAMERA)) CameraSelector.DEFAULT_FRONT_CAMERA
+                else CameraSelector.DEFAULT_BACK_CAMERA
+
+            startCamera()
+        }
+
+        cameraBinding.closeCamera.setOnClickListener { finish() }
+
+        outputDirectory = getOutputDirectory()
+
+
+
     }
+
+    private fun allPermissionGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            this, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull().let {
+            File(
+                it, resources.getString(R.string.app_name)
+            ).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    private fun takePhoto() {
+        //Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        //Create time stamped output file to hold the image
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(
+                FILENAME_FORMAT, Locale.US
+            ).format(System.currentTimeMillis()) + ".jpg"
+        )
+
+        //Create ouput options object which contains file + metaData
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        //Set up image capture listener, which is triggered after photo has been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    onImageCaptured(savedUri)
+                    val msg = "Photo capture succeeded: $savedUri"
+                    //Toast.makeText(this@CameraActivity, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                }
+            })
+    }
+
+    @SuppressLint("UnsafeOptInUsageError")
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            //Used to bind lifecycle of camera to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            //Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(cameraBinding.viewFinder.createSurfaceProvider())
+                }
+
+            imageCapture = ImageCapture.Builder()
+                .build()
+
+            //Select back camera as a default
+            //val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            val viewPort = ViewPort.Builder(Rational(350, 170), Surface.ROTATION_0).build()
+            val useCaseGroup = UseCaseGroup.Builder()
+                .addUseCase(preview)
+                //.addUseCase(imageAnalyzer)
+                .addUseCase(imageCapture!!)
+                .setViewPort(viewPort)
+                .build()
+
+            try {
+                //Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                //Bind use case to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, useCaseGroup
+                )
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture
+                )
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun onImageCaptured(uri: Uri) {
+        val file = File(uri.path!!)
+        imageFile = file
+
+        Glide.with(binding.image).load(file).into(binding.image)
+        showImage()
+    }
+
+    private fun showImage() {
+        binding.borderView.visibility = View.GONE
+        binding.cameraView.root.visibility = View.GONE
+        binding.image.visibility = View.VISIBLE
+    }
+
+
+    companion object {
+        private const val TAG = "AddTaskDialog"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+
 }
--- app/src/main/java/com/dicoding/picodiploma/haidentist/ui/camera/upload/UploadFragment.kt
+++ app/src/main/java/com/dicoding/picodiploma/haidentist/ui/camera/upload/UploadFragment.kt
@@ -1,12 +1,24 @@
 package com.dicoding.picodiploma.haidentist.ui.camera.upload
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.dicoding.picodiploma.haidentist.R
 import com.dicoding.picodiploma.haidentist.databinding.FragmentUploadBinding
+import com.dicoding.picodiploma.haidentist.ui.auth.sign.SignActivity
+import com.dicoding.picodiploma.haidentist.ui.camera.camerax.CameraActivity
+import com.dicoding.picodiploma.haidentist.utils.rotateBitmap
+import java.io.File
 
 
 class UploadFragment : Fragment() {
@@ -16,6 +28,12 @@ class UploadFragment : Fragment() {
 
     private var _binding: FragmentUploadBinding? = null
     val binding get() = _binding!!
+    private var getFile: File? = null
+
+    //User give permission
+    private fun allPermissionGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +48,53 @@ class UploadFragment : Fragment() {
         _binding = FragmentUploadBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        binding.cameraView.setOnClickListener {
+            val intent = Intent(requireContext(), CameraActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.continueCamera.setOnClickListener{
+            val intent = Intent(requireContext(), CameraActivity::class.java)
+
+            if (!allPermissionGranted()) {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    REQUIRED_PERMISSIONS,
+                    REQUEST_CODE_PERMISSIONS
+                )
+                return@setOnClickListener
+            }
+            startActivity(intent)
+        }
 
         return view
+
+    }
+
+    private val launcherIntentCameraX =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == CAMERA_X_RESULT) {
+                val myFile: File = it.data?.getSerializableExtra(EXTRA_PHOTO) as File
+                val backCamera = it.data?.getBooleanExtra(BACK_CAMERA, true) as Boolean
+                getFile = myFile
+
+                val result = if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.O) {
+                    BitmapFactory.decodeFile(myFile.path)
+                } else {
+                    rotateBitmap(BitmapFactory.decodeFile(myFile.path), backCamera)
+                }
+
+                binding.gambarGigis.setImageBitmap(result)
+            }
+        }
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+        const val EXTRA_PHOTO = "extra_photo"
+        const val BACK_CAMERA = "extra_BackCamera"
+
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
     }
 
 
